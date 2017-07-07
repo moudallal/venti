374cbf1cf239c7d5e46dbc6e68e3a7e223d8eb1d
Mohamad Moudallal <mhmicrodev@gmail.com>
2017-07-07 14:19:31 +0300

    Blank Template

--- js/script.js
+++ js/script.js
@@ -1,20 +1,5 @@
 ( function( window ) {
 
-    var $aboutBtn = $('#about'),
-        $teamBtn = $('#team'),
-        $aboutSection = $('.about-section'),
-        $teamSection = $('.team-section');
-
-    var display = function(section){
-		var barHeight = document.getElementById('nav').offsetHeight;
-		$('html, body').animate({scrollTop: section.offset().top - barHeight}, 1000);
-	};
-
-    $aboutBtn.click(function(){
-        display($aboutSection);
-    });
-    $teamBtn.click(function(){
-        display($teamSection);
-    });
+    
 
 })( window );
