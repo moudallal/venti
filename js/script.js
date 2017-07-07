6088d55559450fce97ee3381acb0522c504c22fe
Mohamad Moudallal <mhmicrodev@gmail.com>
2017-07-07 17:32:53 +0300

    Brand new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 ( function( window ) {
 
-    
+    var scrollTop = 0;
+    $(window).scroll(function(){
+
+        scrollTop = $(window).scrollTop();
+        $('.counter').html(scrollTop);
+
+        if (scrollTop >= 90) {
+            $('.navbar').addClass('shadowy-nav');
+        } else if (scrollTop < 90) {
+            $('.navbar').removeClass('shadowy-nav');
+        }
+
+    });
 
 })( window );
